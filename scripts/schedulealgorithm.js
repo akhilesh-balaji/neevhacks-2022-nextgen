8e7d3a7bde60bb0e2106219d3e172dee06b4bafd
Akhilesh Balaji <69246693+akhilesh-balaji@users.noreply.github.com>
2022-03-12 21:28:17 +0530

    Added schedule sequencing algorithm 🎉 Co-authored-by: adhavankumar <adhavankumar@users.noreply.github.com>

--- scripts/schedulealgorithm.js
+++ scripts/schedulealgorithm.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 const computeSchedule = () => {
+    console.log("this is working");
     const task1 = document.getElementById("text-input-item-1").value;
     const task2 = document.getElementById("text-input-item-2").value;
     const task3 = document.getElementById("text-input-item-3").value;
@@ -27,11 +28,94 @@ const computeSchedule = () => {
     const task3_time = parseInt(document.getElementById("time-input-hrs3").value);
     const task4_time = parseInt(document.getElementById("time-input-hrs4").value);
 
-    tasks = [task1, task2, task3, task4];
-    tasks_sct = [task1_sct, task2_sct, task3_sct, task4_sct];
-    tasks_priority = [task1_priority, task2_priority, task3_priority, task4_priority];
+    let tasks = [task1, task2, task3, task4];
+    let tasks_sct = [task1_sct, task2_sct, task3_sct, task4_sct];
+    let tasks_priority = [task1_priority, task2_priority, task3_priority, task4_priority];
+    let tasks_time = [task1_time, task2_time, task3_time, task4_time];
 
     console.log(tasks);
     console.log(tasks_sct);
     console.log(tasks_priority);
+
+    let sortedTasks = tasks.sort((a, b) => {
+        return tasks_priority.indexOf(a) - tasks_priority.indexOf(b);
+    });
+
+    // if there are two or more recurring tasks using sct, then put one non-sct tasks in between
+
+    let sortedTasksSct = tasks_sct.sort((a, b) => {
+        return tasks_priority.indexOf(a) - tasks_priority.indexOf(b);
+    });
+
+    let sortedTasksTime = tasks_time.sort((a, b) => {
+        return tasks_priority.indexOf(a) - tasks_priority.indexOf(b);
+    });
+
+    let schedule = [];
+    let item = [];
+
+    schedule.push(sortedTasks[0]);
+
+    const sortScheduleSct = () => {
+        for (let i = 1; i < sortedTasksSct.length; i++) {
+            console.log(i);
+            if ((sortedTasksSct[i] != sortedTasksSct[i - 1]) && (schedule.indexOf(sortedTasks[i]) == -1)) {
+                schedule.push(sortedTasks[i]);
+            } else {
+                item.push(sortedTasks[i]);
+                for (let j = 1; i + j < sortedTasksSct.length; j++) {
+                    console.log(i + j, sortedTasksSct.length);
+                    console.log(sortedTasksSct[i + j], sortedTasksSct.length);
+                    if (sortedTasksSct[i + j] != sortedTasksSct[i - 1]) {
+                        if (schedule.indexOf(sortedTasks[i + j]) == -1) { // Add this check to item array
+                            schedule.push(sortedTasks[i + j]);
+                            if (schedule.indexOf(item[0]) == -1)
+                                schedule.push(item[0]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // const sortScheduleSct = () => {
+    //     for (let i = 1; i < sortedTasksSct.length; i++) {
+    //         console.log(i);
+    //         if (sortedTasksSct[i] != sortedTasksSct[i - 1]) { // not equal to SCHEDULE
+    //             if (schedule.indexOf(sortedTasks[i]) == -1) {
+    //                 schedule.push(sortedTasks[i]);
+    //             }
+    //         } else {
+    //             item.push(sortedTasks[i]);
+    //             for (let j = 1; i + j < sortedTasksSct.length; j++) {
+    //                 console.log(i + j, sortedTasksSct.length);
+    //                 console.log(sortedTasks[i + j], sortedTasksSct.length);
+    //                 if (sortedTasksSct[i + j] != sortedTasksSct[i - 1]) { // not equal to SCHEDULE
+    //                     if (schedule.indexOf(sortedTasks[i + j]) == -1) {
+    //                         schedule.push(sortedTasks[i + j]);
+    //                         schedule.push(item[0]);
+    //                         break;
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    // [a, b, c, d]
+
+    sortScheduleSct();
+
+    console.log(schedule);
 }
+
+// const sortScheduleSct = () => {
+//     for (let i = 0; i < sortedTasksSct.length; i++) {
+//         if (sortedTasksSct[i] != sortedTasksSct[i - 1]) {
+//             return (sortedTasks[i]);
+//         } else {
+//             return (i + 1);
+//         }
+//     }
+// }
